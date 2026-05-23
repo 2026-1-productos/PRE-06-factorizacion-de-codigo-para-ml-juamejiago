8999eb21ffbe4939903de5e1af798c5f8fa23164
Juan Pablo Mejía Gómez <juamejiago@unal.edu.co>
2026-05-23 11:31:57 -0500

    complete homework 6

--- homework/train_knn.py
+++ homework/train_knn.py
@@ -6,55 +6,25 @@
 #
 
 # importacion de librerias
-import pandas as pd
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
+from src.trainers import select_best_knn
 
-# descarga de datos
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-df = pd.read_csv(url, sep=";")
-
-# preparacion de datos
-y = df["quality"]
-x = df.copy()
-x.pop("quality")
-
-# dividir los datos en entrenamiento y testing
-(x_train, x_test, y_train, y_test) = train_test_split(
-    x,
-    y,
-    test_size=0.25,
-    random_state=123456,
-)
+NEIGHBORS = [1, 3, 5, 7, 9, 11, 15, 21]
 
 # entrenar el modelo
-estimator = KNeighborsRegressor(n_neighbors=5)
-estimator.fit(x_train, y_train)
+result = select_best_knn(NEIGHBORS)
+estimator = result.estimator
 
 print()
 print(estimator, ":", sep="")
 
-# Metricas de error durante entrenamiento
-y_pred = estimator.predict(x_train)
-mse = mean_squared_error(y_train, y_pred)
-mae = mean_absolute_error(y_train, y_pred)
-r2 = r2_score(y_train, y_pred)
-
 print()
 print("Metricas de entrenamiento:")
-print(f"  MSE: {mse}")
-print(f"  MAE: {mae}")
-print(f"  R2: {r2}")
+print(f"  MSE: {result.train_metrics.mse}")
+print(f"  MAE: {result.train_metrics.mae}")
+print(f"  R2: {result.train_metrics.r2}")
 
-# Metricas de error durante testing
 print()
 print("Metricas de testing:")
-y_pred = estimator.predict(x_test)
-mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-print(f"  MSE: {mse}")
-print(f"  MAE: {mae}")
-print(f"  R2: {r2}")
+print(f"  MSE: {result.test_metrics.mse}")
+print(f"  MAE: {result.test_metrics.mae}")
+print(f"  R2: {result.test_metrics.r2}")
